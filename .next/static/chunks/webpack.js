19f975bf201e62fbe4c9275d0b87e064780d3d6f
Ahmed <ahmed.@Ahmeds-MacBook-Pro.local>
2021-08-13 10:29:17 -0600

    edit the url env file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f17928597120b425c82"; }
+/******/ 		__webpack_require__.h = function() { return "d9ca4092658685b4497d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
